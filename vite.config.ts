fc15398d05aa235e4cbcb9dcb02125d0593b7bed
rayguillian <102601811+rayguillian@users.noreply.github.com>
2025-05-10 12:25:17 +0200

    Replace content with files from rayguillian/kangen-v1

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
-import path from 'path';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/kangen-v1/",
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
+  },
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
   },
 });
